e30e6f3149259cbfd3becf7d8f87c851b5756060
GreenSalad420 <sebastianwojszczyk@gmail.com>
2024-06-30 18:33:13 +0200

    Add --logs flag to skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
 if [[ $1 == "--date" ]]; then
     date
+elif [[ $1 == "--logs" ]]; then
+    num_files=${2:-100}
+    for ((i=1; i<=num_files; i++))
+    do
+        echo "Log number: $i" > log_$i.txt
+        echo "Script name: skrypt.sh" >> log_$i.txt
+        echo "Date: $(date)" >> log_$i.txt
+    done
 fi
